b4f36f1fa71574db39fd4aadd20162f7c748cfc1
Daniel Werthén <danielwerthen@gmail.com>
2016-01-18 17:56:47 +0100

    fix(comments): bump version plz

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { composeComponent, exposeContextTypes, renderChild } from './config';
 /**
  * aggregates a set of functions/objects into a constant part + a dynamic part
  *
+ *
  **/
 
 const mergeObjArr = (arr) => {
